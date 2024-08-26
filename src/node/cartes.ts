db5f1933ab732d31e6c4fc69521e01950dcdcf94
Shmuel Melamud <smelamud@redhat.com>
2024-08-27 01:43:07 +0300

    Query cartes with the new API.

--- src/node/cartes.ts
+++ src/node/cartes.ts
@@ -1,4 +1,4 @@
-import { CarteInfo } from "./types";
+import { CarteAttributes, CarteInfo, Scope } from "./types";
 import { CarteSource } from "./caller";
 import { MoeraNode } from "./node";
 
@@ -16,30 +16,45 @@ export class MoeraCartesError extends Error {
 
 }
 
-function isAdminCarte(carte: CarteInfo): boolean {
-    return carte.permissions == null || carte.permissions.includes("other");
-}
-
 /**
  * Class that gets cartes from the given node, caches them and supplies them for authentication.
  */
 export class MoeraCarteSource implements CarteSource {
 
-    private node: MoeraNode;
+    private readonly node: MoeraNode;
+    private readonly clientScope: Scope[];
+    private readonly adminScope: Scope[];
+    private readonly targetNodeName: string | null;
     private cartes: CarteInfo[] = [];
 
     /**
      * @param {MoeraNode} node node to get cartes from
+     * @param {Scope[] | null} clientScope permissions to be granted to the cartes; if not set, all permissions of
+     * the cartes' owner are granted
+     * @param {Scope[] | null} adminScope additional administrative permissions (of those granted to the cartes' owner
+     * by the target node) to be granted to the cartes
+     * @param {string | null} targetNodeName if set, the cartes are valid for authentication on the specified node only
      */
-    constructor(node: MoeraNode) {
+    constructor(
+        node: MoeraNode, clientScope: Scope[] | null = null, adminScope: Scope[] | null = null,
+        targetNodeName: string | null = null
+    ) {
         this.node = node;
+        this.clientScope = clientScope ?? ["all"];
+        this.adminScope = adminScope ?? [];
+        this.targetNodeName = targetNodeName
     }
 
     /**
      * Force renewing the cached list of cartes.
      */
     async renew(): Promise<void> {
-        this.cartes = (await this.node.getCartes()).cartes;
+        const attributes: CarteAttributes = {
+            clientScope: this.clientScope,
+            adminScope: this.adminScope,
+            nodeName: this.targetNodeName
+        };
+        this.cartes = (await this.node.createCartes(attributes)).cartes;
     }
 
     /**
@@ -50,7 +65,7 @@ export class MoeraCarteSource implements CarteSource {
     async getCarte(): Promise<string> {
         for (const renewed of [false, true]) {
             const now = Math.floor(Date.now() / 1000);
-            this.cartes = this.cartes.filter(c => c.deadline > now && isAdminCarte(c));
+            this.cartes = this.cartes.filter(c => c.deadline > now);
             if (this.cartes.length === 0) {
                 if (renewed) {
                     throw new MoeraCartesError("Could not obtain a valid carte from the node");
