9981fe23b9047239db5c9972a7c1941aff1c1d16
Shmuel Melamud <smelamud@redhat.com>
2026-02-23 03:58:11 +0200

    Added Yandex to the list of search engines.

--- src/node/types.ts
+++ src/node/types.ts
@@ -72,7 +72,7 @@ export type SearchContentUpdateType = "block" | "comment-add" | "comment-update"
     | "posting-update-heading" | "posting-update-media-text" | "posting-delete" | "reaction-add" | "reaction-delete"
     | "reactions-delete-all" | "subscribe" | "unblock" | "unfriend" | "unsubscribe";
 
-export type SearchEngine = "google" | "bing";
+export type SearchEngine = "google" | "bing" | "yandex";
 
 export type SearchEntryType = "all" | "posting" | "comment";
 
